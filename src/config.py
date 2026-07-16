9b0c5f5985c50fe7e094f3ef0c7ec323ef6f1629
denisepll <psouza.asped@gmail.com>
2026-07-16 05:49:19 -0300

    Corrige erro de digitação no print

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 # Configuração inicial para todos os notebooks
 
-
 # Importação das bibliotecas
 import pandas as pd
 import numpy as np
@@ -8,11 +7,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-
 # Configuração do visual global
 sns.set_theme(style="whitegrid", palette="muted")
 plt.rcParams["figure.figsize"] = (12, 6)
 plt.rcParams["axes.titlesize"] = 14
 plt.rcParams["axes.labelsize"] = 12
 
-print("n\=== CONFIGURAÇÕES VISUAIS E BLIBLIOTECAS IMPORTADAS COM SUCESSO ===")
+print("n\=== CONFIGURAÇÕES E BIBLIOTECAS IMPORTADAS COM SUCESSO ===")
